8463d0d226c987694ff4d74525b33575da7ed8f6
Mingkun Huang <mingkunhuang95@gmail.com>
2018-04-22 23:08:17 +0800

    loss time test

---
+++ tests/test.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <stdexcept>
+#include <vector>
+#include <limits>
+#include <numeric>
+
+
+#include <rnnt.h>
+
+inline void throw_on_error(rnntStatus_t status, const char* message) {
+    if (status != RNNT_STATUS_SUCCESS) {
+        throw std::runtime_error(message + (", stat = " + 
+                                            std::string(rnntGetStatusString(status))));
+    }
+}
+
+std::vector<float> genActs(int size);
+std::vector<int> genLabels(int alphabet_size, int L);
+
+float rel_diff(const std::vector<float>& grad,
+               const std::vector<float>& num_grad) {
+    float diff = 0.;
+    float tot = 0.;
+    for(size_t idx = 0; idx < grad.size(); ++idx) {
+        diff += (grad[idx] - num_grad[idx]) * (grad[idx] - num_grad[idx]);
+        tot += grad[idx] * grad[idx];
+    }
+
+    return diff / tot;
+}
+
+// Numerically stable softmax for a minibatch of 1
+void softmax(const float* const acts,
+             int alphabet_size, int T,
+             float *probs, bool applylog) {
+
+    for (int t = 0; t < T; ++t) {
+
+        float max_activation =
+            -std::numeric_limits<float>::infinity();
+
+        for (int a = 0; a < alphabet_size; ++a)
+            max_activation =
+               std::max(max_activation, acts[t*alphabet_size + a]);
+
+        float denom = 0;
+        for (int a = 0; a < alphabet_size; ++a)
+            denom += std::exp(acts[t*alphabet_size + a] - max_activation);
+
+        for (int a = 0; a < alphabet_size; ++a) {
+            probs[t*alphabet_size + a] =
+               std::exp(acts[t*alphabet_size + a] - max_activation) / denom;
+            if (applylog) {
+                probs[t*alphabet_size + a] = std::log(probs[t*alphabet_size + a]);
+            }
+        }
+    }
+}
---
+++ tests/test_time.cpp
@@ -0,0 +1,98 @@
+#include <cmath>
+#include <cstdlib>
+#include <random>
+#include <tuple>
+#include <vector>
+
+#include <chrono>
+
+#include <iostream>
+
+#include <rnnt.h>
+
+#include "test.h"
+
+bool run_test(int B, int T, int L, int A, int num_threads) {
+    std::mt19937 gen(2);
+
+    std::vector<float> acts = genActs(B * T * (L + 1) * A);
+
+    std::vector<std::vector<int>> labels;
+    std::vector<int> sizes;
+
+    for (int mb = 0; mb < B; ++mb) {
+        labels.push_back(genLabels(A, L));
+        sizes.push_back(T);
+    }
+
+    std::vector<int> flat_labels;
+    std::vector<int> label_lengths;
+    for (const auto& l : labels) {
+        flat_labels.insert(flat_labels.end(), l.begin(), l.end());
+        label_lengths.push_back(l.size());
+    }
+
+    std::vector<float> costs(B);
+
+    std::vector<float> grads(acts.size());
+
+    rnntOptions options{};
+    options.maxT = T;
+    options.maxU = L + 1;
+    options.blank_label = 0;
+    options.batch_first = true;
+    options.loc = RNNT_CPU;
+    options.num_threads = num_threads;
+
+    size_t cpu_alloc_bytes;
+    throw_on_error(get_workspace_size(T, L+1, B,
+                                     false,
+                                     &cpu_alloc_bytes),
+                    "Error: get_workspace_size in run_test");
+    
+    void* rnnt_cpu_workspace = malloc(cpu_alloc_bytes);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    throw_on_error(compute_rnnt_loss(acts.data(), grads.data(),
+                                    flat_labels.data(), label_lengths.data(),
+                                    sizes.data(),
+                                    A, B,
+                                    costs.data(),
+                                    rnnt_cpu_workspace,
+                                    options),
+                    "Error: compute_ctc_loss (0) in run_test");
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "compute_ctc_loss elapsed time: " << elapsed.count() * 1000 << " ms\n";
+
+    float cost = std::accumulate(costs.begin(), costs.end(), 0.);
+
+    free(rnnt_cpu_workspace);
+}
+
+int main(int argc, char** argv) {
+    if (argc < 5) {
+        std::cerr << "Arguments: <Batch size> <Time step> <Label length> <Alphabet size>\n";
+        return 1;
+    }
+
+    int B = atoi(argv[1]);
+    int T = atoi(argv[2]);
+    int L = atoi(argv[3]);
+    int A = atoi(argv[4]);
+    std::cout << "Arguments: " \
+                << "\nBatch size: " << B \
+                << "\nTime step: " << T \
+                << "\nLabel length: " << L \
+                << "\nAlphabet size: " << A \
+                << std::endl;
+    
+    int num_threads = 1;
+    if (argc >= 6) {
+        num_threads = atoi(argv[5]);
+        std::cout << "Num threads: " << num_threads << std::endl;
+    }
+
+    run_test(B, T, L, A, num_threads);
+}

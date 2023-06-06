81158bd9ff330290ac6fabae103ffabc18cbb229
yanbing-j <yanbing.jiang@intel.com>
2023-06-06 15:49:20 +0800

    Update c++17 for pytorch 2.1.0

--- pytorch_binding/setup.py
+++ pytorch_binding/setup.py
@@ -8,10 +8,10 @@ from torch.utils.cpp_extension import BuildExtension, CppExtension
 
 
 extra_compile_args = ['-fPIC']
-if LooseVersion(torch.__version__) >= LooseVersion("1.5.0"):
-    extra_compile_args += ['-std=c++14']
+if LooseVersion(torch.__version__) >= LooseVersion("2.1.0"):
+    extra_compile_args += ['-std=c++17']
 else:
-    extra_compile_args += ['-std=c++11']
+    extra_compile_args += ['-std=c++14']
 warp_rnnt_path = "../build"
 
 if torch.cuda.is_available() or "CUDA_HOME" in os.environ:
